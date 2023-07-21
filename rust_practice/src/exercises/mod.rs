ad2ae9f8889d4065961aeaf86562acdc88261945
Marwan <ali186@mcmaster.ca>
2023-07-20 21:18:07 -0400

    rust_practice -> points_and_polygons

--- rust_practice/src/exercises/mod.rs
+++ rust_practice/src/exercises/mod.rs
@@ -1,2 +1,3 @@
 pub mod health_statistics;
 pub mod storing_books;
+pub mod points_and_polygons;
--- rust_practice/target/debug/deps/rust_practice-345a20ceeb339e0e.d
+++ rust_practice/target/debug/deps/rust_practice-345a20ceeb339e0e.d
@@ -1,8 +1,9 @@
-/home/marwan/Coding-Practice/rust_practice/target/debug/deps/rust_practice-345a20ceeb339e0e.rmeta: src/main.rs src/exercises/mod.rs src/exercises/storing_books.rs src/exercises/health_statistics.rs
+/home/marwan/Coding-Practice/rust_practice/target/debug/deps/rust_practice-345a20ceeb339e0e.rmeta: src/main.rs src/exercises/mod.rs src/exercises/health_statistics.rs src/exercises/storing_books.rs src/exercises/points_and_polygons.rs
 
-/home/marwan/Coding-Practice/rust_practice/target/debug/deps/rust_practice-345a20ceeb339e0e.d: src/main.rs src/exercises/mod.rs src/exercises/storing_books.rs src/exercises/health_statistics.rs
+/home/marwan/Coding-Practice/rust_practice/target/debug/deps/rust_practice-345a20ceeb339e0e.d: src/main.rs src/exercises/mod.rs src/exercises/health_statistics.rs src/exercises/storing_books.rs src/exercises/points_and_polygons.rs
 
 src/main.rs:
 src/exercises/mod.rs:
-src/exercises/storing_books.rs:
 src/exercises/health_statistics.rs:
+src/exercises/storing_books.rs:
+src/exercises/points_and_polygons.rs:
